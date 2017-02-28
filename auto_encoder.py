a7274d61d19dc542d7295bd8df8b0b42a759d3f0
Zhihang Fan <canoefzh@gmail.com>
2017-02-28 10:43:04 +0800

    bug fix

--- auto_encoder.py
+++ auto_encoder.py
@@ -3,7 +3,7 @@ import sklearn.preprocessing as prep
 import tensorflow as tf 
 from tensorflow.examples.tutorials.mnist import input_data
 
-def xavief_init(fan_in, fan_out, constant = 1):
+def xavier_init(fan_in, fan_out, constant = 1):
     low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
     high = -low
     return tf.random_uniform((fan_in, fan_out), minval = low, maxval = high, dtype = tf.float32)
@@ -26,7 +26,7 @@ class AdditvieGaussianNoiseAutoencoder(object):
     		self.weights['w2']),
     		self.weights['b2'])
 
-    	self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, slef.x), 2.0))
+    	self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction, self.x), 2.0))
     	self.optimizer = optimizer.minimize(self.cost)
 
     	init = tf.global_variables_initializer()
@@ -45,7 +45,7 @@ class AdditvieGaussianNoiseAutoencoder(object):
     	return all_weights
 
     def partial_fit(self, X):
-    	cost, opt = self.sess.run((sefl.cost, self.optimizer), feed_dict = {self.x: X, self.scale: self.training_scale})
+    	cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict = {self.x: X, self.scale: self.training_scale})
     	return cost
 
     def calc_total_cost(self, X):
@@ -69,19 +69,19 @@ class AdditvieGaussianNoiseAutoencoder(object):
     	return self.sess.run(sefl.weights['b1'])
 
 
-minst = input_data.read_data_sets('MNIST_data', one_hot = True)
+mnist = input_data.read_data_sets('MNIST_data', one_hot = True)
 
 def standard_scale(X_train, X_test):
     preprocessor = prep.StandardScaler().fit(X_train)
     X_train = preprocessor.transform(X_train)
-    X_test = preprocessor.transform(X.test)
+    X_test = preprocessor.transform(X_test)
     return X_train, X_test
 
-def get_random_block_from_data(daa, batch_size):
+def get_random_block_from_data(data, batch_size):
     start_index = np.random.randint(0, len(data) - batch_size)
     return data[start_index: (start_index + batch_size)]
 
-X_train, X_text = standard_scale(mnist.train.images, minst.test.images)
+X_train, X_text = standard_scale(mnist.train.images, mnist.test.images)
 n_samples = int(mnist.train.num_examples)
 traning_epochs = 20
 batch_size = 128
@@ -89,7 +89,7 @@ display_step = 1
 
 autoencoder = AdditvieGaussianNoiseAutoencoder(n_input = 784, n_hidden = 200, transfer_function = tf.nn.softplus, optimizer = tf.train.AdamOptimizer(learning_rate = 0.001), scale = 0.01)
 
-for epoch in range(training_scale):
+for epoch in range(traning_epochs):
     avg_cost = 0
     total_batch = int(n_samples / batch_size)
     for i in range(total_batch):
@@ -102,5 +102,5 @@ for epoch in range(training_scale):
     if epoch % display_step == 0:
     	print ('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
 
-print ('Total const: ' + str(autoencoder.calc_total_cost(x_test)))
+print ('Total const: ' + str(autoencoder.calc_total_cost(X_test)))
 
